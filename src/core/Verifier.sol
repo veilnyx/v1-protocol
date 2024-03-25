f130aaa0b331ad2c7e5ae50ffe169c862d45875c
nvnx7 <thenvnsahu@gmail.com>
2024-03-26 00:21:39 +0530

    chore: cleanup

--- src/core/Verifier.sol
+++ src/core/Verifier.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
-import {console2} from "forge-std/console2.sol";
 import {IVerifier} from "../interfaces/IVerifier.sol";
 import {VerifierInfo} from "../libraries/DataTypes.sol";
 import {ZTransaction, ZTransactionType, ZTransactionLogic} from "../libraries/ZTransaction.sol";
