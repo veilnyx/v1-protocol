44e2ccc2b0a75c98613f3bb72fb61aa5d5f37605
nvnx7 <thenvnsahu@gmail.com>
2024-04-03 02:29:33 +0530

    test: add paymaster validation test

--- test/Paymaster.t.sol
+++ test/Paymaster.t.sol
@@ -3,6 +3,9 @@ pragma solidity 0.8.23;
 
 import {Test} from "forge-std/Test.sol";
 import {EntryPoint} from "@account-abstraction/contracts/core/EntryPoint.sol";
+import {PackedUserOperation} from "@account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import {Pool} from "src/core/Pool.sol";
+import {ZTransaction, ZTransactionType} from "src/libraries/ZTransaction.sol";
 import {Paymaster} from "src/core/Paymaster.sol";
 
 contract PaymasterTest is Test {
@@ -10,14 +13,18 @@ contract PaymasterTest is Test {
     address public entryPoint;
     Paymaster public paymaster;
 
+    uint24 defaultAssetId = 65537;
+    uint256 defaultFeeValue = 0.1 ether;
+
     function setUp() public {
         mockPool = address(uint160(uint256(keccak256("pool"))));
         entryPoint = address(new EntryPoint());
         paymaster = new Paymaster(entryPoint, mockPool);
+        paymaster.updateAssetFee(defaultAssetId, defaultFeeValue);
     }
 
     function test_updateFeeAsset() public {
-        uint24 assetId = 65537;
+        uint24 assetId = 65538;
         uint256 feeValue = 0.1 ether;
         paymaster.updateAssetFee(assetId, feeValue);
         assertEq(paymaster.assetFees(assetId), feeValue);
@@ -45,5 +52,34 @@ contract PaymasterTest is Test {
 
         assertEq(newDeposit, value - withdrawValue);
         assertEq(withdrawAddress.balance, withdrawValue);
+    }
+
+    function test_validatePaymasterUserOp() public {
+        ZTransaction memory ztx;
+        PackedUserOperation memory userOp;
+
+        ztx.pubAssetIds = new uint24[](1);
+        ztx.pubAssetIds[0] = defaultAssetId;
+        ztx.feeData = uint256(
+            bytes32(
+                bytes.concat(
+                    bytes20(address(paymaster)),
+                    bytes12(uint96(0.1 ether))
+                )
+            )
+        );
+
+        userOp.sender = mockPool;
+        userOp.callData = abi.encodeCall(Pool.transact, (ztx));
+
+        vm.startPrank(entryPoint);
+        (, uint256 flag) = paymaster.validatePaymasterUserOp(
+            userOp,
+            bytes32(0),
+            0
+        );
+        vm.stopPrank();
+
+        assertEq(flag, 0);
     }
 }
