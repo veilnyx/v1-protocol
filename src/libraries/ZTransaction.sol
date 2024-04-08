c657182e153862af4727cf220c5396c0a244680f
nvnx7 <thenvnsahu@gmail.com>
2024-04-09 04:25:33 +0530

    feat: add functions & incl. input memos field in `ZTransaction`

--- src/libraries/ZTransaction.sol
+++ src/libraries/ZTransaction.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.23;
 
+import {PoseidonT4} from "poseidon-solidity/PoseidonT4.sol";
+import {IPool} from "../interfaces/IPool.sol";
+import {IVerifier} from "../interfaces/IVerifier.sol";
+import {IConvertor} from "../interfaces/IConvertor.sol";
+import {Asset, AssetLogic} from "./Asset.sol";
+import {MerkleTree, MerkleTreeLogic} from "./MerkleTree.sol";
+
 uint256 constant FIELD_SIZE = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
 
 enum ZTransactionType {
@@ -8,6 +15,12 @@ enum ZTransactionType {
     TRANSFER,
     WITHDRAW,
     CONVERT
+}
+
+enum MemoType {
+    NULL,
+    SEMI,
+    FULL
 }
 
 struct ZTransaction {
@@ -20,7 +33,8 @@ struct ZTransaction {
     // Notes info
     uint256[] nullifiers;
     uint256[] commitments;
-    bytes[] memos;
+    bytes inMemos;
+    bytes[] outMemos;
     // Fees info
     uint256 feeData; // 20-byte address + 12-byte fee value
     uint256 beneficiary; // stealth address or normal address
@@ -32,6 +46,8 @@ struct ZTransaction {
 }
 
 library ZTransactionLogic {
+    using MerkleTreeLogic for MerkleTree;
+
     function hash(ZTransaction memory self) public pure returns (uint256) {
         return
             uint256(
@@ -43,7 +59,8 @@ library ZTransactionLogic {
                         self.pubValues,
                         self.nullifiers,
                         self.commitments,
-                        self.memos,
+                        self.inMemos,
+                        self.outMemos,
                         self.feeData,
                         self.beneficiary,
                         self.beneficiaryMemo,
@@ -54,10 +71,61 @@ library ZTransactionLogic {
             ) % FIELD_SIZE;
     }
 
+    function execute(
+        ZTransaction memory ztx,
+        MerkleTree storage tree,
+        mapping(uint24 => Asset) storage assets,
+        mapping(address => bool) storage convertProxies,
+        mapping(uint256 => bool) storage markedNullifiers,
+        address verifier,
+        address convertor
+    ) external {
+        _validateTransaction(
+            tree,
+            convertProxies,
+            markedNullifiers,
+            verifier,
+            ztx
+        );
+
+        // Transfer any fees
+        _transferFee(assets, ztx);
+
+        // Receive any deposits
+        if (ztx.txType == ZTransactionType.DEPOSIT) {
+            _receiveAssetsFrom(
+                assets,
+                msg.sender,
+                ztx.pubAssetIds,
+                ztx.pubValues
+            );
+        }
+
+        // Transfer any withdrawals
+        if (ztx.txType == ZTransactionType.WITHDRAW) {
+            _transferToExceptFee(
+                assets,
+                address(uint160(ztx.beneficiary)),
+                ztx
+            );
+        }
+
+        // Perform any conversions
+        if (ztx.txType == ZTransactionType.CONVERT) {
+            _transferToExceptFee(assets, convertor, ztx);
+            _convert(assets, convertor, ztx);
+        }
+
+        _mintNotes(tree, ztx.commitments, ztx.inMemos, ztx.outMemos);
+
+        emit IPool.ComplianceMemo(ztx.complianceMemo);
+    }
+
     function toVerifierInput(
         ZTransaction memory self,
         bytes4 selector,
-        uint256[2] memory encryptionPublicKey
+        uint256 encryptionPublicKeyX,
+        uint256 encryptionPublicKeyY
     ) public pure returns (bytes memory) {
         uint256 nIns = self.nullifiers.length;
         uint256 nOuts = self.commitments.length;
@@ -97,8 +165,8 @@ library ZTransactionLogic {
         }
 
         // Compliance encryption key: (Index: (3 + nIns + 3 * nOuts) to (5 + nIns + 3 * nOuts))
-        pubInputs[3 + nIns + 3 * nOuts] = encryptionPublicKey[0];
-        pubInputs[4 + nIns + 3 * nOuts] = encryptionPublicKey[1];
+        pubInputs[3 + nIns + 3 * nOuts] = encryptionPublicKeyX;
+        pubInputs[4 + nIns + 3 * nOuts] = encryptionPublicKeyY;
 
         // Compliance memo: (Index: (5 + nIns + 3 * nOuts) to (9 + nIns + 4 * nOuts))
         // [5 + nIns + 3 * nOuts]: ephemeral pub key x
@@ -118,15 +186,209 @@ library ZTransactionLogic {
             }
         }
 
-        bytes memory packdPubInp = toPackedPubInp(pubInputs);
+        bytes memory packdPubInp = _packPubInputs(pubInputs);
         bytes memory data = bytes.concat(selector, self.proof, packdPubInp);
 
         return data;
     }
 
-    function toPackedPubInp(
+    function _convert(
+        mapping(uint24 => Asset) storage assets,
+        address convertor,
+        ZTransaction memory ztx
+    ) internal {
+        (uint24[] memory outAssetIds, uint256[] memory outValues) = IConvertor(
+            convertor
+        ).convert(
+                ztx.target,
+                ztx.pubAssetIds,
+                ztx.pubValues,
+                ztx.targetPayload
+            );
+
+        _receiveAssetsFrom(assets, convertor, outAssetIds, outValues);
+
+        uint256 cmLen = ztx.commitments.length;
+        uint256 outLen = outAssetIds.length;
+        uint256 newCmLen = cmLen + outLen;
+
+        uint256[] memory newCommitments = new uint256[](newCmLen);
+        bytes[] memory newMemos = new bytes[](newCmLen);
+
+        for (uint8 i = 0; i < cmLen; ) {
+            newCommitments[i] = ztx.commitments[i];
+            newMemos[i] = ztx.outMemos[i];
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        for (uint8 i = 0; i < outLen; ) {
+            newCommitments[i + cmLen] = PoseidonT4.hash(
+                [outAssetIds[i], ztx.beneficiary, outValues[i]]
+            );
+            newMemos[i + cmLen] = abi.encodePacked(
+                MemoType.SEMI,
+                outAssetIds[i],
+                ztx.beneficiary,
+                outValues[i],
+                ztx.beneficiaryMemo
+            );
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        ztx.commitments = newCommitments;
+        ztx.outMemos = newMemos;
+    }
+
+    function _transferFee(
+        mapping(uint24 => Asset) storage assets,
+        ZTransaction memory ztx
+    ) internal {
+        uint256 feeValue = uint256(uint96(ztx.feeData));
+
+        if (feeValue != 0) {
+            address paymaster = address(bytes20(bytes32(ztx.feeData)));
+
+            AssetLogic.transferAsset({
+                assets: assets,
+                to: paymaster,
+                assetId: ztx.pubAssetIds[0],
+                value: feeValue
+            });
+        }
+    }
+
+    function _transferToExceptFee(
+        mapping(uint24 => Asset) storage assets,
+        address to,
+        ZTransaction memory ztx
+    ) internal {
+        uint256 pubAssetCount = ztx.pubAssetIds.length;
+        uint256 feeValue = uint256(uint96(ztx.feeData));
+        uint256 firstValue = ztx.pubValues[0] - feeValue;
+
+        if (firstValue != 0) {
+            AssetLogic.transferAsset({
+                assets: assets,
+                to: to,
+                assetId: ztx.pubAssetIds[0],
+                value: firstValue
+            });
+        }
+
+        for (uint8 i = 1; i < pubAssetCount; ) {
+            AssetLogic.transferAsset({
+                assets: assets,
+                to: to,
+                assetId: ztx.pubAssetIds[i],
+                value: ztx.pubValues[i]
+            });
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    function _receiveAssetsFrom(
+        mapping(uint24 => Asset) storage assets,
+        address from,
+        uint24[] memory assetIds,
+        uint256[] memory values
+    ) internal {
+        uint256 count = assetIds.length;
+        for (uint8 i = 0; i < count; ) {
+            AssetLogic.receiveAsset({
+                assets: assets,
+                from: from,
+                assetId: assetIds[i],
+                value: values[i]
+            });
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    function _validateTransaction(
+        MerkleTree storage tree,
+        mapping(address => bool) storage supportedProxies,
+        mapping(uint256 => bool) storage markedNullifiers,
+        address verifier,
+        ZTransaction memory ztx
+    ) internal {
+        // Check recent merkle root
+        if (!tree.isKnownRoot(ztx.merkleRoot, 100)) {
+            revert IPool.UnknownMerkleRoot();
+        }
+
+        if (
+            ztx.txType == ZTransactionType.CONVERT &&
+            !supportedProxies[ztx.target]
+        ) {
+            revert IPool.UnsupportedProxy();
+        }
+
+        // Verify ZK proof
+        if (!IVerifier(verifier).verifyTransactionProof(ztx)) {
+            revert IPool.InvalidProof();
+        }
+
+        // Check double spend and mark nullifiers
+        _checkAndMarkNullifiers(markedNullifiers, ztx.nullifiers);
+    }
+
+    /// @dev This also prevents any duplicate nullifiers
+    function _checkAndMarkNullifiers(
+        mapping(uint256 => bool) storage markedNullifiers,
+        uint256[] memory nullifiers
+    ) internal {
+        for (uint256 i = 0; i < nullifiers.length; ) {
+            if (markedNullifiers[nullifiers[i]]) {
+                revert IPool.DoubleSpend(nullifiers[i]);
+            }
+
+            markedNullifiers[nullifiers[i]] = true;
+            emit IPool.NullifierMarked(nullifiers[i]);
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    function _mintNotes(
+        MerkleTree storage tree,
+        uint256[] memory commitments,
+        bytes memory inMemos,
+        bytes[] memory outMemos
+    ) internal {
+        uint256 nextIndex = MerkleTreeLogic.insert(tree, commitments);
+        uint256 cmLen = commitments.length;
+
+        for (uint256 i = 0; i < cmLen; ) {
+            emit IPool.Announcement(
+                nextIndex - cmLen + i,
+                commitments[i],
+                outMemos[i]
+            );
+            unchecked {
+                ++i;
+            }
+        }
+
+        emit IPool.InputNoteMemos(inMemos);
+    }
+
+    function _packPubInputs(
         uint256[] memory arr
-    ) public pure returns (bytes memory) {
+    ) internal pure returns (bytes memory) {
         uint256 len = arr.length;
         bytes memory res = new bytes(32 * len);
 
