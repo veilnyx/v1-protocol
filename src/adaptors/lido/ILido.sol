03a114f066a25b9de38993614e37ce816b0d0263
Shivendra Singh <shiventechub@gmail.com>
2024-07-25 18:35:39 +0530

    chores: rename fix

--- src/adaptors/lido/ILido.sol
+++ src/adaptors/lido/ILido.sol
@@ -3,7 +3,9 @@
 pragma solidity 0.8.24;
 
 interface ILido {
-    function submit(address _referral) external payable returns(uint256);
+    function submit(address _referral) external payable returns (uint256);
 
-    function getPooledEthByShares(uint256 _sharesAmount) external view returns (uint256)
+    function getPooledEthByShares(
+        uint256 _sharesAmount
+    ) external view returns (uint256);
 }
--- src/interfaces/IWstEthToken.sol
+++ src/interfaces/IWstEthToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.24;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-interface IWToken is IERC20 {
+interface IWstEthToken is IERC20 {
     function getWstETHByStETH(uint256 _stETHAmount) external returns (uint256);
 
     function getStETHByWstETH(uint256 _wstETHAmount) external returns (uint256);
