57ed573f4ed7c863ff0cbbb073f7148dfb65f619
nvnx7 <thenvnsahu@gmail.com>
2024-08-20 02:24:56 +0700

    chore: config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,29 @@
+import dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-viem";
 import "@nomicfoundation/hardhat-foundry";
-import "@nomicfoundation/hardhat-toolbox-viem";
 import "@nomicfoundation/hardhat-ignition";
+import "@nomicfoundation/hardhat-toolbox-viem";
 import "@nomicfoundation/hardhat-ignition-viem";
+
+dotenv.config();
+
+const rpcEthereumSepolia = process.env.RPC_ETHEREUM_SEPOLIA as string;
+const rpcOptimismSepolia = process.env.RPC_OPTIMISM_SEPOLIA as string;
+const privateKeys = [process.env.PRIVATE_KEY as string];
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    ethereumSepolia: {
+      url: rpcEthereumSepolia,
+      accounts: privateKeys,
+    },
+    optimismSepolia: {
+      url: rpcOptimismSepolia,
+      accounts: privateKeys,
+    },
+  },
 };
 
 export default config;
