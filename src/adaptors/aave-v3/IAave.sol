e8345c8c87985a51a12d305257b4f75c0f2a047a
Shivendra Singh <shiventechub@gmail.com>
2024-08-29 18:08:40 +0530

    feat: aave adaptor

---
+++ src/adaptors/aave-v3/IAave.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity 0.8.24;
+
+/// Interface for Aave Pool.
+/// ETH Sepolia addr: 0x6Ae43d3271ff6888e7Fc43Fd7321a503ff738951
+
+interface IAave {
+    function supply(
+        address asset,
+        uint256 amount,
+        address onBehalfOf,
+        uint16 referralCode
+    ) external;
+
+    function withdraw(
+        address asset,
+        uint256 amount,
+        address to
+    ) external returns (uint256);
+}
