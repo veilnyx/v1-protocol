1ae3899f4047e02468e42b5542528b74dcb52c5a
nvnx7 <thenvnsahu@gmail.com>
2024-11-04 22:48:17 +0530

    chore: configs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,11 @@
-import dotenv from "dotenv";
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-viem";
-import "@nomicfoundation/hardhat-foundry";
-import "@nomicfoundation/hardhat-ignition";
-import "@nomicfoundation/hardhat-ignition-viem";
-import "hardhat-contract-sizer";
+import dotenv from 'dotenv';
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-viem';
+import '@nomicfoundation/hardhat-foundry';
+import '@nomicfoundation/hardhat-ignition';
+import '@nomicfoundation/hardhat-ignition-viem';
+// import '@nomiclabs/hardhat-solhint';
+import 'hardhat-contract-sizer';
 // import * as tdly from "@tenderly/hardhat-tenderly";
 
 // tdly.setup({ automaticVerifications: true });
@@ -15,10 +16,10 @@ const rpcOptimismSepolia = process.env.RPC_OPTIMISM_SEPOLIA as string;
 const rpcTenderlyMainnet = process.env.RPC_TENDERLY_MAINNET as string;
 const rpcTenderlyMainnetCustomId = process.env.RPC_TENDERLY_MAINNET_CUSTOM_ID as string;
 const privateKeys = [process.env.PRIVATE_KEY as string];
-const forkEnabled = process.env.HARDHAT_FORK === "true";
+const forkEnabled = process.env.HARDHAT_FORK === 'true';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: '0.8.24',
   networks: {
     hardhat: {
       allowUnlimitedContractSize: false,
@@ -43,14 +44,14 @@ const config: HardhatUserConfig = {
     tenderlyMainnetCustomId: {
       url: rpcTenderlyMainnetCustomId,
       accounts: privateKeys,
-      chainId: 7800
-    }
+      chainId: 7800,
+    },
   },
   contractSizer: {
     runOnCompile: true,
     strict: true,
-    unit: "kB"
-  }
+    unit: 'kB',
+  },
 };
 
 export default config;
