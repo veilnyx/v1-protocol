55c4e5b92d92e4e965eb8d92c36bcd224c6d66b2
Shivendra Singh <shiventechub@gmail.com>
2025-01-29 20:47:34 +0530

    script: pool proxy upgrade script

--- script/setup/TransferOwnership.s.sol
+++ script/setup/TransferOwnership.s.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "forge-std/Script.sol";
 
-contract TransferOwnershipScript is Script {
+contract TransferOwnership is Script {
     address public newOwner = 0x8f9Af25A446b8fF4aFBb52438e5B60512510fa63;
     address public ownableContractAddress =
-        0x9163043b553aDeF9fE44b088922560cfBFdEC51b;
+        0x7E53C283730C0Fa9d38f263BD1f51cB6B4D68efE;
 
     function run() external {
         uint256 privateKey = vm.envUint("PRIVATE_KEY");
